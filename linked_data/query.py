a5355b0047cde66403b0d526131b3db12112dfe7
Miriam Mendez <miriam.mendez.s@gmail.com>
2024-01-20 10:11:19 +0100

    data management and linked data done

--- linked_data/query.py
+++ linked_data/query.py
@@ -2,17 +2,49 @@ import rdflib
 
 
 def get_devices_of_buildings_query(building):
-    return """
+    return f"""
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+    PREFIX s4blg: <https://saref.etsi.org/saref4bldg/> 
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+    PREFIX ex: <http://example.org/> 
+
+    SELECT ?device ?deviceType
+    WHERE {{
+        {building}  rdf:type s4blg:Building;
+                    s4blg:contains ?device .
+        ?device rdf:type s4blg:Device;
+                ex:tipus ?deviceType .
+    }}
     """
 
 
 def get_all_type_buildings_query(type):
-    return """
+    return f"""
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+    PREFIX s4blg: <https://saref.etsi.org/saref4bldg/> 
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+    PREFIX ex: <http://example.org/> 
+
+    SELECT ?building
+    WHERE {{
+        ?x  rdf:type s4blg:Building;
+            ex:tipus "{type}" ;
+            foaf:name ?building .
+    }}
     """
 
 
 def get_all_buildings_query():
     return """
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+    PREFIX s4blg: <https://saref.etsi.org/saref4bldg/> 
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+
+    SELECT ?building
+    WHERE {
+        ?x  rdf:type s4blg:Building;
+            foaf:name ?building .
+    }
     """
 
 
@@ -27,6 +59,6 @@ if __name__ == '__main__':
     for resp in g.query(get_all_type_buildings_query("Oficina")):
         print(resp)
 
-    print("DEVICES AND TYPE OF BUILDING B2")
+    print("DEVICES AND TYPE OF BUILDING B3")
     for resp in g.query(get_devices_of_buildings_query("ex:b3")):
         print(resp)
